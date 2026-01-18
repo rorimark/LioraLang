1e15ecfd2edb09770843ad1d71944eee5eb73049
Mark Storchovyi <markstorchovyi@gmail.com>
2026-01-18 16:55:57 +0100

    refactor(router): add redirecting when page is not found and refactored imports of page components

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,15 +1,18 @@
+import NotFoundPage from "../components/pages/NotFoundPage/NotFoundPage";
 import HomePage from "../components/pages/HomePage/HomePage";
-import FlashcardsPage from "../components/pages/FlashcardsPage";
-import LearnPage from "../components/pages/LearnPage";
-import TestPage from "../components/pages/TestPage";
-import SettingsPage from "../components/pages/SettingsPage";
+import FlashcardsPage from "../components/pages/FlashcardsPage/FlashcardsPage";
+import LearnPage from "../components/pages/LearnPage/LearnPage";
+import TestPage from "../components/pages/TestPage/TestPage";
+import SettingsPage from "../components/pages/SettingsPage/SettingsPage";
 import homeIcon from "../assets/icons/home-icon.svg";
 import cardsIcon from "../assets/icons/cards.svg";
 import bookIcon from "../assets/icons/book-icon.svg";
 import testIcon from "../assets/icons/test-icon.svg";
 import settingsIcon from "../assets/icons/settings-icon.svg";
+import { Navigate } from "react-router-dom";
 
 export const ROUTES = [
+  { path: "*", element: <Navigate to="/" replace /> },
   { path: "/", element: <HomePage />, label: "Home", icon: homeIcon },
   {
     path: "/flashcards",
